41b330b12142f1b3a0ff9a69b547a501b753b4a8
Limetail <60202087+Limetail@users.noreply.github.com>
2020-01-29 19:12:58 -0800

    Add files via upload

---
+++ Ch 3 - 1 Exercise/Ch3 - 1 pseudocode/Ch3 - 1 pseudocode.cpp
@@ -0,0 +1,9 @@
+/*-- Input Phase --
+Print prompt "Enter radius: "
+Read Radius
+
+-- Process Phase --
+CircleArea = 3.1416 * Radius ** 2
+
+-- Output Phase --
+Display CircleArea*/
---
+++ Ch 3 - 2/Ch3 - 2/Ch3 - 2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Modified version of original C program "
+        << " (by Kernighan & Ritchie)\n";
+
+    std::cout << "Hello World!";
+
+    return 0;
+}
